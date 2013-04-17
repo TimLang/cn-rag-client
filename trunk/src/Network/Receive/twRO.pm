686d8a1def53e78eca7c678a039715e7590f1011
esanisa@gmail.com <esanisa@gmail.com@c7626e61-8c77-c3c0-e1ae-84baa1a7423d>
2013-04-17 18:07:36 +0000

    修正生命条matk问题 同步twRO封包(少了点ST里面的东西)

--- trunk/src/Network/Receive/twRO.pm
+++ trunk/src/Network/Receive/twRO.pm
@@ -20,17 +20,19 @@ use Globals;
 use base qw(Network::Receive::ServerType0);
 use Log qw(message warning error debug);
 use Network::MessageTokenizer;
-use I18N qw(bytesToString stringToBytes);
+use Misc;
 use Utils qw(timeOut getHex);
+use Translation;
+use I18N qw(bytesToString stringToBytes);
 
 sub new {
 	my ($class) = @_;
 	my $self = $class->SUPER::new(@_);
 	my %packets = (
-		'006D' => ['character_creation_successful', 'a4 V9 v V2 v14 Z24 C6 v2', [qw(charID exp zeny exp_job lv_job opt1 opt2 option stance manner points_free hp hp_max sp sp_max walk_speed type hair_style weapon lv points_skill lowhead shield tophead midhead hair_color clothes_color name str agi vit int dex luk slot renameflag)]],
+		'006D' => ['character_creation_successful', 'a4 V9 v V2 v14 Z24 C6 v2 Z*', [qw(charID exp zeny exp_job lv_job opt1 opt2 option stance manner points_free hp hp_max sp sp_max walk_speed type hair_style weapon lv points_skill lowhead shield tophead midhead hair_color clothes_color name str agi vit int dex luk slot renameflag mapname)]],
 		'0097' => ['private_message', 'v Z28 Z*', [qw(len privMsgUser privMsg)]],
-		'082D' => ['characters_slots_info', 'x2 C5 x20', [qw(normal_slot premium_slot billing_slot producible_slot valid_slot)]],
-		'08B9' => ['second_passwd_login', 'x4 a4 v', [qw(accountID unknown)]], # 12
+		'082D' => ['characters_slots_info', 'v C5 x20', [qw(packet_len normal_slot premium_slot billing_slot producible_slot valid_slot)]],
+		'08B9' => ['login_pin_code_request', 'V a4 v', [qw(seed accountID flag)]], # len: 12
 	);
 
 	foreach my $switch (keys %packets) {
@@ -54,6 +56,8 @@ sub new {
 
 sub sync_received_characters {
 	my ($self, $args) = @_;
+	$charSvrSet{sync_Count} = $args->{sync_Count} if (exists $args->{sync_Count});
+
 	if ($config{'XKore'} ne '1') {
 		$messageSender->sendToServer($messageSender->reconstruct({switch => 'sync_received_characters'}));
 	}
@@ -62,7 +66,7 @@ sub sync_received_characters {
 sub characters_slots_info {
 	my ($self, $args) = @_;
 
-	$charSvrSet{total_slot} = $args->{total_slot} if (exists $args->{total_slot});
+	$charSvrSet{packet_len} = $args->{packet_len} if (exists $args->{packet_len});
 	$charSvrSet{normal_slot} = $args->{normal_slot} if (exists $args->{normal_slot});
 	$charSvrSet{premium_slot} = $args->{premium_slot} if (exists $args->{premium_slot});
 	$charSvrSet{billing_slot} = $args->{billing_slot} if (exists $args->{billing_slot});
@@ -72,12 +76,66 @@ sub characters_slots_info {
 	$timeout{charlogin}{time} = time;
 }
 
-# This is ten second-level password for 2013/3/29 upgrading of twRO
-sub second_passwd_login {
+# This is ten second-level password login for 2013/3/29 upgrading of twRO
+sub login_pin_code_request {
 	my ($self, $args) = @_;
 
-	my $accountID = $args->{accountID};
-	debug sprintf("Account ID: %s (%s)\n", unpack('V',$accountID), getHex($accountID));
+	if (($args->{seed} == 0) && ($args->{flag} == 0)) {
+		message T("PIN code is correct.\n"), "success";
+		# call charSelectScreen
+		$self->{lockCharScreen} = 0;
+		if (charSelectScreen(1) == 1) {
+			$firstLoginMap = 1;
+			$startingzeny = $chars[$config{'char'}]{'zeny'} unless defined $startingzeny;
+			$sentWelcomeMessage = 1;
+		}
+	} elsif ($args->{flag} == 1) {
+		# PIN code query request.
+		$accountID = $args->{accountID};
+		debug sprintf("Account ID: %s (%s)\n", unpack('V',$accountID), getHex($accountID));
+
+		message T("Server requested PIN password in order to select your character.\n"), "connection";
+		return if ($config{loginPinCode} eq '' && !($self->queryAndSaveLoginPinCode()));
+		$messageSender->sendLoginPinCode($args->{seed}, 0);
+	} elsif ($args->{flag} == 2) {
+		# PIN code has never been set before, so set it.
+		warning T("PIN password is not set for this account.\n"), "connection";
+		return if ($config{loginPinCode} eq '' && !($self->queryAndSaveLoginPinCode()));
+
+		while ((($config{loginPinCode} =~ /[^0-9]/) || (length($config{loginPinCode}) != 4)) &&
+		  !($self->queryAndSaveLoginPinCode("Your PIN should never contain anything but exactly 4 numbers.\n"))) {
+			error T("Your PIN should never contain anything but exactly 4 numbers.\n");
+		}
+		$messageSender->sendLoginPinCode($args->{seed}, 1);
+	} elsif ($args->{flag} == 5) {
+		# PIN code invalid.
+		error T("PIN code is invalid, don't use sequences or repeated numbers.\n");
+		configModify('loginPinCode', '', 1);
+		return if (!($self->queryAndSaveLoginPinCode(T("The login PIN code that you entered is invalid. Please re-enter your login PIN code."))));
+		$messageSender->sendLoginPinCode($args->{seed}, 0);
+	} elsif ($args->{flag} == 7) {
+		# PIN code disabled.
+		$accountID = $args->{accountID};
+		debug sprintf("Account ID: %s (%s)\n", unpack('V',$accountID), getHex($accountID));
+
+		# call charSelectScreen
+		$self->{lockCharScreen} = 0;
+		if (charSelectScreen(1) == 1) {
+			$firstLoginMap = 1;
+			$startingzeny = $chars[$config{'char'}]{'zeny'} unless defined $startingzeny;
+			$sentWelcomeMessage = 1;
+		}
+	} elsif ($args->{flag} == 8) {
+		# PIN code incorrect.
+		error T("PIN code is incorrect.\n");
+		configModify('loginPinCode', '', 1);
+		return if (!($self->queryAndSaveLoginPinCode(T("The login PIN code that you entered is incorrect. Please re-enter your login PIN code."))));
+		$messageSender->sendLoginPinCode($args->{seed}, 0);
+	} else {
+		debug("login_pin_code_request: unknown flag $args->{flag}\n");
+	}
+
+	$timeout{master}{time} = time;
 }
 
 1;
