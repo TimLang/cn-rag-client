1b15a4e905549fd396f6bec170bb9a9742ddeee5
cnkorecoco@gmail.com <cnkorecoco@gmail.com@c7626e61-8c77-c3c0-e1ae-84baa1a7423d>
2013-04-19 17:13:29 +0000

    修复传送之阵！

--- trunk/plugins/autowarpn.pl
+++ trunk/plugins/autowarpn.pl
@@ -1,26 +1,31 @@
-# autowarpn (completely new code by Joseph)
-# licensed under gpl v2
-# ported to 2.x by Kissa2k
-
 package autowarpn;
 
 use strict;
 use Globals;
-use Log qw(message);
+use Log qw(message error);
 use Utils;
 use Network::Send;
 use Misc;
 use AI;
+use Translation;
+use encoding 'utf8';
 
 Plugins::register('autowarpn', 'Auto warp before walk to lockmap.', \&unload);
 
 my $hooks = Plugins::addHooks(
-   ['AI_pre', \&AI_hook],
-   ['is_casting', \&casting_hook],
-   ['parseMsg/pre', \&packet_hook],
+	['AI_pre', \&AI_hook],
+	['packet/warp_portal_list', \&warplist_hook],
+	['packet/skill_use_location', \&warpopen_hook],
+	['packet/skill_use_failed', \&warpfailed_hook],
+	['Network::Receive::map_changed', \&mapchange_hook],
 );
 
 my $cHook = Log::addHook(\&cHook);
+my $warpOpened;
+my $warpNowOpen;
+my $movewarp;
+my $warpfailed;
+my $warpoff = 0;
 
 sub unload {
    Plugins::delHooks($hooks);
@@ -28,79 +33,114 @@ sub unload {
 
 sub cHook {
    my $type = shift;
-    my $domain = shift;
+   my $domain = shift;
    my $level = shift;
    my $currentVerbosity = shift;
    my $message = shift;
    
-   if ($message =~ /计算路径至坐标/ &&
+   if ($message =~ /计算路径至坐标/ && $warpoff == 0 &&
      existsInList($config{autoWarp_from}, $field->baseName) &&
      $char->{skills}{AL_WARP} && $char->{skills}{AL_WARP}{lv} > 0) {
       AI::queue("autowarp");
-      AI::args->{timeout} = 5;
+      AI::args->{timeout} = 1;
       AI::args->{time} = time;
       AI::args->{map} = $field->baseName;
-      message "Preparing to cast a warp portal to $config{autoWarp_to}\n";
+      message "准备使用传送之阵至地图 $config{autoWarp_to}\n";
+	  $warpOpened = 1;
    }
 }
 
 sub AI_hook {
-   my $hookName = shift;
+	my $hookName = shift;
 
-   if (AI::action eq "autowarp") {
-      if ($field->baseName ne AI::args->{map} || $field->name ne AI::args->{map}) {
-         AI::dequeue;
-         return;
-      }
-      if (timeOut(AI::args)) {
-         my $pos = getEmptyPos($char, 4);
-         $messageSender->sendSkillUseLoc(27, 4, $pos->{x}, $pos->{y});
-         stopAttack();
-         message "Attempting to open warp portal at $pos->{x} $pos->{y}\n";
-         AI::args->{timeout} = 15;
-         AI::args->{time} = time;
-      }
-   }
+	if ($warpOpened == 1) {
+		AI::dequeue if (AI::action eq "route");
+		if (AI::action eq "autowarp" && timeOut(AI::args)) {
+			if ($field->baseName ne AI::args->{map} || $field->name ne AI::args->{map}) {
+			AI::dequeue;
+			return;
+		} else {
+			my $pos = getEmptyPos($char, 4);
+			$messageSender->sendSkillUseLoc(27, 4, $pos->{x}, $pos->{y});
+			$warpOpened = 0;
+#			stopAttack();
+			message "尝试在 $pos->{x} $pos->{y} 打开传送之门!\n";
+			AI::args->{timeout} = 15;
+			AI::args->{time} = time;
+			}
+		}
+	}
+	
+	if (AI::action eq "autowarp" && $warpOpened == 0 && $warpfailed == 1) {
+		$warpfailed = 0;
+		$warpoff = 1;
+		AI::dequeue;
+		return;
+	}
 }
 
-sub packet_hook {
-   my $hookName = shift;
-   my $args = shift;
-   my $switch = $args->{switch};
-   my $msg = $args->{msg};
-
-   if ($switch eq "011C" && $args->{skillID} eq 27) {
-      $messageSender->sendWarpTele(27, $config{'autoWarp_to'}.".gat");
-   }
+sub warplist_hook {
+	if (AI::action eq "autowarp") {
+		for (my $i = 0; $i < @{$char->{warp}{memo}}; $i++) {
+			last if ($char->{warp}{memo}[$i] eq $config{autoWarp_to});
+			if (($i == @{$char->{warp}{memo}} - 1) && ($char->{warp}{memo}[$i] ne $config{autoWarp_to})) {
+				error TF("您并未记录 '%s' 传送点! 再次使用自动传送之门需重启!!!\n", $config{autoWarp_to});
+				$warpfailed = 1;
+			} 
+		}
+		$messageSender->sendWarpTele(27, $config{autoWarp_to}.".gat");
+		$warpNowOpen = 1;
+	}
 }
 
-sub casting_hook {
-   my $hookName = shift;
-   my $args = shift;
+sub warpopen_hook {
+	my ($hookname, $args) = @_;
 
-   # it's our warp portal! ok lets go in
-   if ($args->{sourceID} eq $accountID && $args->{skillID} eq 27) {
-      message "Moving into warp portal at $args->{x} $args->{y}\n";
-      main::ai_route($field->baseName, $args->{x}, $args->{y},
-         noSitAuto => 1,
-         attackOnRoute => 0);
-   }
+	if (AI::action eq "autowarp" && $warpfailed != 1) {
+		if ($args->{sourceID} eq $accountID && $args->{skillID} == 27 && $warpNowOpen == 1) {
+		$warpNowOpen = 0;
+		$movewarp = 1;
+		message "到 $args->{x} $args->{y} 进入传送之阵!\n";
+		main::ai_route($field->baseName, $args->{x}, $args->{y},
+		noSitAuto => 1,
+		attackOnRoute => 0);
+		}
+	}
+}
+
+sub mapchange_hook {
+	my $ai_string;
+	if (@ai_seq) {
+		$ai_string =  join(' ', @ai_seq);
+	}
+	if ($ai_string =~ /autowarp/) {
+			AI::clear;
+			return;
+	}
+}
+
+sub warpfailed_hook {
+	my ($hookname, $args) = @_;
+
+	if ($movewarp == 1) {
+		if ($args->{skillID} == 27 && $args->{type} == 8) {
+		error "您身上已没有蓝色魔力矿石了! 再次使用自动传送之门需重启!!!\n";
+		$movewarp = 0;
+		$warpfailed = 1;
+		}
+	}
 }
 
 sub getEmptyPos {
    my $obj = shift;
    my $maxDist = shift;
 
-   # load info about everyone's location
    my %pos;
    for (my $i = 0; $i < @playersID; $i++) {
       next if (!$playersID[$i]);
       my $player = $players{$playersID[$i]};
       $pos{$player->{pos_to}{x}}{$player->{pos_to}{y}} = 1;
    }
-
-   # crazy algorithm i made for spiral scanning the area around you
-   # i wont bother to document it since im lazy and it already confuses me
 
    my @vectors = (-1, 0, 1, 0);
 
@@ -118,7 +158,6 @@ sub getEmptyPos {
                my $pos = {x=>$posx, y=>$posy};
                return $pos if checkLineWalkable($obj->{pos_to}, $pos);
             }
-
             $posx += $vectors[$vecx];
             $posy += $vectors[$vecy];
          }
