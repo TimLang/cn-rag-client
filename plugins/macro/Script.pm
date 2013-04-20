9f97e471cec0eb5f4edbc2a659206598452692e3
esanisa <esanisa@gmail.com>
2013-04-20 17:36:08 +0000

    修复macro中正则的问题 官方8510-8511

--- plugins/macro/Script.pm
+++ plugins/macro/Script.pm
@@ -263,7 +263,7 @@ sub next {
 				$self->{line}++;
 				$searchEnd = ${$macro{$self->{name}}}[$self->{line}];
 				
-				if ($searchEnd =~ /^if.*{/) {
+				if ($searchEnd =~ /^if\s+\(.*\).*{/) {
 					$countBlockIf++;
 				} elsif (($searchEnd eq '}') || ($searchEnd =~ /}\s*else\s*{/ && $countBlockIf == 1)) {
 					$countBlockIf--;
@@ -291,7 +291,7 @@ sub next {
 				$self->{line}++;
 				my $searchEnd = ${$macro{$self->{name}}}[$self->{line}];
 				
-				if ($searchEnd =~ /^if.*{/) {
+				if ($searchEnd =~ /^if\s+\(.*\).*{/) {
 					$countBlockIf++;
 				} elsif (($searchEnd eq '}') ||
 					($countBlockIf == 1 && ($searchEnd =~ /}\s*else\s*{/ || $searchEnd =~ /}\s*elsif\s+\(\s*(.*)\s*\).*/))) {
